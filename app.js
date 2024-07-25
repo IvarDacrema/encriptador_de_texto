c1d73b8724db42833fd11335074fb4329913ff38
Ivar Dacrema <ivar.m.dacrema@gmail.com>
2024-07-25 13:11:51 -0300

    Eliminar img y texto con el boton desencriptar

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 let introduccion = document.getElementById('textoEntrada');
+let resultado = document.getElementById('caja_resultado')
+let cajaResultado = document.getElementsByClassName('caja_resultado_contenedor')
 
 function limpiar(){
     introduccion.innerHTML = "";
 }
 
+function encriptar(){
+    console.log(introduccion.value.replaceAll("a", 'boca'))
+    console.log(cajaResultado.value);
+}
+
+function desencriptar(){
+    resultado.innerHTML = "";
+}
