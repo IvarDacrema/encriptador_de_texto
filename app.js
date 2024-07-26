a56ae6a64344210fd75f16d137c87410553f3cce
Ivar Dacrema <ivar.m.dacrema@gmail.com>
2024-07-26 13:29:35 -0300

    documentacion del encriptador

--- app.js
+++ app.js
@@ -4,31 +4,42 @@ let imagen = document.getElementById('id_resultado_img');
 let texto = document.getElementById('id_resultado_texto')
 let resultado = document.getElementById('contenedor_resultado')
 let cajaResultado = document.getElementsByClassName('caja_resultado_contenedor')
+let addEtiqueta = document.createElement("textarea");
 let textoResultado = "";
+let controlDeEntrada = 0;
 
 function limpiar(){
+    //borra el contenido de la caja donde se introduce el texto que queremos encriptar al hacer click en ella
     introduccion.innerHTML = "";
 }
 
 function encriptar(){
-    textoResultado = introduccion.value.replaceAll("a", 'boca');
-    resultado.innerHTML = "";
-    let respuestaResultado = document.createElement("textarea");
-    respuestaResultado.setAttribute('id', 'textoAreaRespuesta')
-    let contenidoRespuesta = document.createTextNode(textoResultado);
-    respuestaResultado.appendChild(contenidoRespuesta);
-    document.body.insertBefore(respuestaResultado, resultado);
-    console.log(contenidoRespuesta)
+    if(controlDeEntrada==0){
+        //guarda el texto modificado con replaceAll en texto resultado
+        textoResultado = introduccion.value.replaceAll("a", 'boca');
+        //añade a addEtiqueta el atributo class para poder modificarlo en el css
+        addEtiqueta.setAttribute('class', 'texto_area_respuesta');
+        //añade el atributo hidden a los mensajes que estan debajo de la imagen en la caja de respuesta para que no se vea a la hora de apretar el boton encriptar
+        texto.setAttribute('hidden', '');
+        //se crea el contenido que va a tener el textarea creado en las variables
+        let contenidoRespuesta = document.createTextNode(textoResultado);
+        //se le introduce el texto creado a la etiqueta textarea
+        addEtiqueta.appendChild(contenidoRespuesta);
+        //remplaza la imagen de la caja de respuestas con nuestro textarea con el texto encriptado
+        resultado.replaceChild(addEtiqueta, imagen);
+    } 
+    if(controlDeEntrada>0){
+        //borramos el contenido del textarea
+        addEtiqueta.innerHTML = "";
+        //se crea nuevo contenido para mostrar en textarea respuesta
+        textoResultado = document.createTextNode(introduccion.value.replaceAll("a", 'boca'));
+        //se añade a el contenido nuevo a textarea respuesta
+        addEtiqueta.appendChild(textoResultado);
+    }
+    //suma al contador para que no vuelva a hacer el replazo de los hijos del nodo y solo haga la encriptacion
+    controlDeEntrada++;
 }
 
 function desencriptar(){
-    textoResultado = introduccion.value.replaceAll("a", 'boca');
-    let addEtiqueta = document.createElement("textarea");
-    addEtiqueta.setAttribute('class', 'texto_area_respuesta')
-    texto.setAttribute('hidden', '')
-    let contenidoRespuesta = document.createTextNode(textoResultado);
-    addEtiqueta.appendChild(contenidoRespuesta);
-    console.log(addEtiqueta);
-    resultado.replaceChild(addEtiqueta, imagen);
-    console.log(contenidoRespuesta)
+    
 }
