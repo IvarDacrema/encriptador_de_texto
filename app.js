107dca3e865e6bb3d75c03dd971520833215bdaa
Ivar Dacrema <ivar.m.dacrema@gmail.com>
2024-07-26 12:26:36 -0300

    Mejorando cuadro de respuesta y encriptacion

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 let introduccion = document.getElementById('textoEntrada');
-let resultado = document.getElementById('caja_resultado')
+let contenedorPadre = document.getElementById('contenedor_padre')
+let imagen = document.getElementById('id_resultado_img');
+let texto = document.getElementById('id_resultado_texto')
+let resultado = document.getElementById('contenedor_resultado')
 let cajaResultado = document.getElementsByClassName('caja_resultado_contenedor')
+let textoResultado = "";
 
 function limpiar(){
     introduccion.innerHTML = "";
 }
 
 function encriptar(){
-    console.log(introduccion.value.replaceAll("a", 'boca'))
-    console.log(cajaResultado.value);
+    textoResultado = introduccion.value.replaceAll("a", 'boca');
+    resultado.innerHTML = "";
+    let respuestaResultado = document.createElement("textarea");
+    respuestaResultado.setAttribute('id', 'textoAreaRespuesta')
+    let contenidoRespuesta = document.createTextNode(textoResultado);
+    respuestaResultado.appendChild(contenidoRespuesta);
+    document.body.insertBefore(respuestaResultado, resultado);
+    console.log(contenidoRespuesta)
 }
 
 function desencriptar(){
-    resultado.innerHTML = "";
+    textoResultado = introduccion.value.replaceAll("a", 'boca');
+    let addEtiqueta = document.createElement("textarea");
+    addEtiqueta.setAttribute('class', 'texto_area_respuesta')
+    texto.setAttribute('hidden', '')
+    let contenidoRespuesta = document.createTextNode(textoResultado);
+    addEtiqueta.appendChild(contenidoRespuesta);
+    console.log(addEtiqueta);
+    resultado.replaceChild(addEtiqueta, imagen);
+    console.log(contenidoRespuesta)
 }
