3c027acd932905be9c350d9d80122ac63ec1c9f6
Ivar Dacrema <ivar.m.dacrema@gmail.com>
2024-08-01 22:46:20 -0300

    intento de desencriptar

--- app.js
+++ app.js
@@ -7,19 +7,32 @@ let cajaResultado = document.getElementsByClassName('caja_resultado_contenedor')
 let addEtiqueta = document.createElement("textarea");
 let textoCopiado = "";
 let textoResultado = "";
+let contenidoRespuesta = "";
 let controlDeEntrada = 0;
 
 // Tabla de mapeo de vocales a otros caracteres
-let mapasCambio = {
-  'a': 'ai',
-  'e': 'enter',
-  'i': 'imes',
-  'o': 'ober', 
-  'u': 'ufat'
-};
+let mapasCambio = new Map([
+    ['a', 'ai'],
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+  ]);
 
-// Expresión regular para coincidir con todas las vocales (tanto minúsculas como mayúsculas)
+//Funcion para invertir las llaves y valores para desencriptar
+function invertirMap(map) {
+    let invertedMap = new Map();
+    for (let [key, value] of map) {
+        invertedMap.set(value, key);
+    }
+    return invertedMap;
+}
+
+// Expresión regular para coincidir con todas las vocales
 let letrasCambiar = /[aeiou]/g;
+
+//Expresion para coincidir con las palabras claves
+let letrasEncriptadas = /ai|enter|imes|ober|ufat/g;
 
 function limpiar(){
     //borra el contenido de la caja donde se introduce el texto que queremos encriptar al hacer click en ella
@@ -36,7 +49,7 @@ function encriptar(){
         //añade a addEtiqueta el atributo class para poder modificarlo en el css
         addEtiqueta.setAttribute('class', 'texto_area_respuesta');
         //se crea el contenido que va a tener el textarea creado en las variables
-        let contenidoRespuesta = document.createTextNode(textoResultado);
+        contenidoRespuesta = document.createTextNode(textoResultado);
         //se le introduce el texto creado a la etiqueta textarea
         addEtiqueta.appendChild(contenidoRespuesta);
         //remplaza la imagen de la caja de respuestas con nuestro textarea con el texto encriptado
@@ -53,9 +66,13 @@ function encriptar(){
         //borramos el contenido del textarea
         addEtiqueta.innerHTML = "";
         //se crea nuevo contenido para mostrar en textarea respuesta
-        textoResultado = document.createTextNode(introduccion.value.replaceAll("a", 'boca'));
+        textoResultado = introduccion.value.replaceAll(letrasCambiar, (match) => {
+            // Reemplazamos cada vocal según la tabla de mapeo
+            return mapasCambio[match];
+          });
+        contenidoRespuesta = document.createTextNode(textoResultado);
         //se añade a el contenido nuevo a textarea respuesta
-        addEtiqueta.appendChild(textoResultado);
+        addEtiqueta.appendChild(contenidoRespuesta);
     }
     //suma al contador para que no vuelva a hacer el replazo de los hijos del nodo y solo haga la encriptacion
     controlDeEntrada++;
@@ -64,10 +81,15 @@ function encriptar(){
 function desencriptar(){
     //borramos el contenido del textarea
     addEtiqueta.innerHTML = "";
+    //obtenemos el mapa invertido
+    let mapaInvertido = invertirMap(mapasCambio);
     //se crea nuevo contenido para mostrar en textarea respuesta desencriptada
-    textoResultado = document.createTextNode(introduccion.value.replaceAll("boca", 'a'));
+    textoResultado = introduccion.value.replaceAll(letrasEncriptadas, (match) => {
+        // Reemplazamos cada cadena encriptada según el mapa invertido
+        return mapaInvertido[match];
+    });
     //se añade a el contenido nuevo a textarea respuesta
-    addEtiqueta.appendChild(textoResultado);
+    addEtiqueta.appendChild(document.createTextNode(textoResultado));
 }
 
 function copiar(){
